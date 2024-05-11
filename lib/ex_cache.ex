6e68cd11bd825164ff0265e9011b55dbbf38711c
tt67wq <tt67wq@aliyun.com>
2024-05-11 23:13:26 +0800

    init

---
+++ lib/ex_cache.ex
@@ -0,0 +1,18 @@
+defmodule ExCache do
+  @moduledoc """
+  Documentation for `ExCache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCacheTest do
+  use ExUnit.Case
+  doctest ExCache
+
+  test "greets the world" do
+    assert ExCache.hello() == :world
+  end
+end
